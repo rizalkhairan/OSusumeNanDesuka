4fa6a841a71441a21a25b4f2431a31b4c917f7f3
Staryo40 <arrrryow@gmail.com>
2025-04-07 05:33:23 +0700

    feat: keyboard header add new definitions for arrow keys

--- src/header/keyboard/keyboard.h
+++ src/header/keyboard/keyboard.h
@@ -23,6 +23,11 @@
 
 #define CAPS_LOCK 0x3A
 
+#define KEY_UP     0x80
+#define KEY_DOWN   0x81
+#define KEY_LEFT   0x82
+#define KEY_RIGHT  0x83
+
 #define KEY_RELEASE(scancode) ((scancode) + 0x80)
 ////
 
