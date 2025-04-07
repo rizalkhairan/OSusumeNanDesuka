87f0b6a2f786062ca3701470a0b2e47b70546e8d
Staryo40 <arrrryow@gmail.com>
2025-04-07 13:17:02 +0700

    feat: remove duplicate from terminal history and fix other minor bugs

--- src/header/terminal/terminal.h
+++ src/header/terminal/terminal.h
@@ -17,6 +17,7 @@ typedef struct {
     TerminalLine history[MAX_HISTORY];
     int hist_length;
     int current_line; 
+    int viewed_line;
     int current_line_row;
     int current_line_col;
     int cursor_row;
@@ -25,6 +26,8 @@ typedef struct {
 
 void terminal_initialize();
 void terminal_handle_input(char);
+void add_line_to_history();
+bool is_same_line(TerminalLine, TerminalLine);
 void terminal_line_insert_char(TerminalLine*, char, int);
 void terminal_line_delete_char(TerminalLine*, int);
 
