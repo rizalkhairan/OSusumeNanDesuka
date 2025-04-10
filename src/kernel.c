7eb38a96fcd88c8adf9c5fc92d566019938ac056
Sanesasaha <13523066@std.stei.itb.ac.id>
2025-04-10 17:02:27 +0700

    feat: disk template files and makefile

--- src/kernel.c
+++ src/kernel.c
@@ -6,6 +6,8 @@
 #include "header/kernel-entrypoint.h"
 #include "header/text/framebuffer.h"
 #include "header/keyboard/keyboard.h"
+#include "header/terminal/terminal.h"
+#include "header/filesystem/disk.h"
 
 void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
@@ -14,6 +16,11 @@ void kernel_setup(void) {
     activate_keyboard_interrupt();
     terminal_initialize();
     keyboard_state_activate();
+
+    struct BlockBuffer b;
+    for (int i = 0; i < 512; i++) b.buf[i] = i % 16;
+    write_blocks(&b, 17, 1);
+    while (true);
 
     while (true) {
         char c;
