a0ff7251403d29971c577c1cc33a0b03fd83ac1c
Sanesasaha <13523066@std.stei.itb.ac.id>
2025-05-22 18:00:31 +0700

    feat: paging and process struct for process

--- src/header/process/process.h
+++ src/header/process/process.h
@@ -45,9 +45,48 @@
 #define PROCESS_CREATE_FAIL_NOT_ENOUGH_MEMORY    3
 #define PROCESS_CREATE_FAIL_FS_READ_FAILURE      4
 
+/**
+ * Contain information needed for task to be able to get interrupted and resumed later
+ *
+ * @param cpu                         All CPU register state
+ * @param eip                         CPU instruction counter to resume execution
+ * @param eflags                      Flag register to load before resuming the execution
+ * @param page_directory_virtual_addr CPU register CR3, containing pointer to active page directory
+ */
+struct Context {
+    // TODO: Add important field here
+    struct CPURegister cpu;
+    uint32_t eip;
+    uint32_t eflags;
+    struct PageDirectory* page_directory_virtual_addr;
+};
 
+typedef enum PROCESS_STATE {
+    READY,
+    RUNNING,
+    BLOCKED
+} PROCESS_STATE;
 
+/**
+ * Structure data containing information about a process
+ *
+ * @param metadata Process metadata, contain various information about process
+ * @param context  Process context used for context saving & switching
+ * @param memory   Memory used for the process
+ */
+struct ProcessControlBlock {
+    struct {
+        uint32_t pid;
+        PROCESS_STATE process_state;
+    } metadata;
 
+    struct Context context;
+
+    struct {
+        void     *virtual_addr_used[PROCESS_PAGE_FRAME_COUNT_MAX];
+        uint32_t page_frame_used_count;
+    } memory;
+};
 
 /**
  * Get currently running process PCB pointer
